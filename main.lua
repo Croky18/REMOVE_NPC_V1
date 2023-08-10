edede469a38fc38acfd85a0125ab7b81e4fe68d2
Croky <141557212+Croky18@users.noreply.github.com>
2023-08-10 12:32:50 +0200

    main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,85 @@
---Gemaakt door croky#8472--
+local currentVersion = "1.0" -- Huidige versie van je script
+local scriptUrl = "https://raw.githubusercontent.com/yourusername/yourrepository/master/script.lua" -- URL naar je GitHub-scriptbestand
+
+local function CheckForScriptUpdate()
+    PerformHttpRequest(scriptUrl, function(errorCode, resultData, resultHeaders)
+        if errorCode == 200 then
+            local lines = {}
+            for line in resultData:gmatch("[^\r\n]+") do
+                table.insert(lines, line)
+            end
+
+            local remoteVersion = lines[1]
+            if remoteVersion and remoteVersion ~= currentVersion then
+                print("Er is een nieuwe versie beschikbaar: " .. remoteVersion)
+                print("Update beschikbaar op: " .. scriptUrl)
+            else
+                print("Je hebt de nieuwste versie van het script.")
+            end
+        else
+            print("Fout bij het controleren op updates.")
+        end
+    end, "GET", "", {})
+end
 
 Citizen.CreateThread(function()
-    while (true) do
-		ClearAreaOfPeds(-446.2, 6013.42, 31.72, 170.63, 1)
-        Citizen.Wait(0)
+    while true do
+        Citizen.Wait(60000) -- Controleer elke minuut op updates
+
+        CheckForScriptUpdate()
     end
 end)
----------------------------------add meer hier onder/ADD MORE--------------------------------------------------------------
+
+-- Rest van je script hier...
+
+local zones = {
+    {
+        name = "zone1",
+        center = vector3(-445.17, 6014.54, 31.72),
+        distance = 16.0, -- Maximale afstand om als "in de zone" te worden beschouwd
+        npcDelete = true, -- NPC's verwijderen in deze zone
+        vehicleDelete = false, -- Geparkeerde voertuigen verwijderen in deze zone
+    },
+    
+    -- Voeg meer zones toe zoals hierboven
+}
+
 Citizen.CreateThread(function()
-    while (true) do
-		ClearAreaOfPeds(00.0, 00.0, 00.0, 00.0, 1) ---locatie 
-        Citizen.Wait(0)
+    while true do
+        Citizen.Wait(0) -- Wacht 1 seconde voordat opnieuw te controleren
+
+        local playerCoords = GetEntityCoords(PlayerPedId())
+        
+        for _, zone in ipairs(zones) do
+            local distanceToZone = #(zone.center - playerCoords)
+
+            if distanceToZone <= zone.distance then
+                DeleteEntitiesInZone(zone)
+            end
+        end
     end
 end)
+
+function DeleteEntitiesInZone(zone)
+    if zone.npcDelete then
+        local peds = GetGamePool('CPed')
+        for _, ped in ipairs(peds) do
+            local pedCoords = GetEntityCoords(ped)
+            local distanceToZone = #(zone.center - pedCoords)
+            if distanceToZone <= zone.distance then
+                DeleteEntity(ped)
+            end
+        end
+    end
+
+    if zone.vehicleDelete then
+        local vehicles = GetGamePool('CVehicle')
+        for _, vehicle in ipairs(vehicles) do
+            local vehicleCoords = GetEntityCoords(vehicle)
+            local distanceToZone = #(zone.center - vehicleCoords)
+            if distanceToZone <= zone.distance and not IsPedInAnyVehicle(vehicle, false) then
+                DeleteEntity(vehicle)
+            end
+        end
+    end
+end
