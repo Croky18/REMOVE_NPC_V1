47b7a0145ed255baee7e4e10c47200d86f429dcb
Croky <141557212+Croky18@users.noreply.github.com>
2023-08-10 14:36:14 +0200

    main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,8 @@
 --gemaakt door croky#8472--> bij problemen DM mijn op discord
 --made by croky#8472--> if you have any problems DM my on discor
 
-local currentVersion = "2.0" -- Huidige versie van je script
-local scriptUrl = "https://github.com/Croky18/clearareaNPC/blob/main/main.lua" -- URL naar je GitHub-scriptbestand
+local currentVersion = "2.0" -- Huidige versie van je script/-- Current version of your script
+local scriptUrl = "https://github.com/Croky18/clearareaNPC/blob/main/main.lua" --Niet Veranderen/--do not change
 
 local function CheckForScriptUpdate()
     PerformHttpRequest(scriptUrl, function(errorCode, resultData, resultHeaders)
@@ -14,20 +14,20 @@ local function CheckForScriptUpdate()
 
             local remoteVersion = lines[1]
             if remoteVersion and remoteVersion ~= currentVersion then
-                print("Er is een nieuwe versie beschikbaar: " .. remoteVersion)
-                print("Update beschikbaar op: " .. scriptUrl)
+                print("A new version is available: " .. remoteVersion)
+                print("Update available on: " .. scriptUrl)
             else
-                print("Je hebt de nieuwste versie van het script.")
+                print("You have the latest version of the script.")
             end
         else
-            print("Fout bij het controleren op updates.")
+            print("Error checking for updates.")
         end
     end, "GET", "", {})
 end
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(60000) -- Controleer elke minuut op updates
+        Citizen.Wait(60000) -- Controleer elke minuut op updates/-- Check for update every minutes
 
         CheckForScriptUpdate()
     end
@@ -37,17 +37,17 @@ local zones = {
     {
         name = "zone1",
         center = vector3(-445.17, 6014.54, 31.72),
-        distance = 16.0, -- Maximale afstand om als "in de zone" te worden beschouwd
-        npcDelete = true, -- NPC's verwijderen in deze zone
-        vehicleDelete = false, -- Geparkeerde voertuigen verwijderen in deze zone
+        distance = 16.0, -- Maximale afstand om als "in de zone" te worden beschouwd/--Maximum distance to be considered "in the zone"
+        npcDelete = false, -- NPC's verwijderen in deze zone/--Remove NPCs in this zone
+        vehicleDelete = false, -- Geparkeerde voertuigen verwijderen in deze zone/--Remove parked vehicles in this zone
     },
     
-    -- Voeg meer zones toe zoals hierboven
+    -- Voeg meer zones toe zoals hierboven/-- Add more zones as above
 }
 
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(0) -- Wacht 1 seconde voordat opnieuw te controleren
+        Citizen.Wait(0) -- Wacht 1 seconde voordat opnieuw te controleren/---- Wait 1 second before checking again
 
         local playerCoords = GetEntityCoords(PlayerPedId())
         
